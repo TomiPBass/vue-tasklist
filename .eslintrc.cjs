578644cb7fd33a488afe9182efbb0242d82d5c4d
Tomas Pizur <Pizik3@seznam.cz>
2022-10-22 18:28:59 +0200

    feat: eslint format on save

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,11 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
     ecmaVersion: "latest",
   },
+  rules: [
+    
+  ]
 };
---
+++ src/components/TheTaskPlain.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todo_plain">
+    <div class="todo_grid">
+      <TheTask v-for="task in storeTasks.tasks" :key="task.id" :task="task" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+// IMPORTS
+
+import TheTask from "@/components/TheTask.vue";
+import { useStoreTasks } from "@/stores/counter";
+
+// CONSTS
+
+const storeTasks = useStoreTasks();
+</script>
