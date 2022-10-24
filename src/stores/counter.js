d3436ea30561ca9b0d0b607229c1343c9004d36f
Tomas Pizur <Pizik3@seznam.cz>
2022-10-24 14:03:12 +0200

    fix: formating changes

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -14,7 +14,7 @@ export const useStoreTasks = defineStore("storeTasks", {
           id: "id2",
           title: "Go to sleep bitch",
           date: "2nd of March",
-          description: "Zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+          description: "Zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzzzzzzzzzz zzzzzzzzzzzzzzzzzzzzz"
         }
       ]
     }
@@ -34,24 +34,25 @@ export const useStoreTasks = defineStore("storeTasks", {
       let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
       [mydate.getMonth()];
       let dayAdder = "";
+      let day = mydate.getDate();
+      let dayStr = day.toString();
       const getAdder = () => {
-          let day = mydate.getDate();
-          let dayStr = day.toString();
           let dayArr= dayStr.split("");
           console.log(dayArr);
-          if (dayArr[(dayArr.length - 1)] === "1") {
+          if (dayArr[(dayArr.length - 1)] === "1" && dayArr[(dayArr.length - 2)] !== "1") {
               dayAdder = "st"
-          } else if (dayArr[(dayArr.length - 1)] === "2") {
+          } else if (dayArr[(dayArr.length - 1)] === "2" && dayArr[(dayArr.length - 2)] !== "1") {
               dayAdder = "nd"
-          } else if (dayArr[(dayArr.length - 1)] === "3") {
+          } else if (dayArr[(dayArr.length - 1)] === "3" && dayArr[(dayArr.length - 2)] !== "1") {
               dayAdder = "rd"
           } else {
               dayAdder = "th"
           }
           return dayAdder;
       }
+      
       getAdder();
-      const date = mydate.getDate() + dayAdder + ' of ' + month + ' ' + mydate.getFullYear();
+      const date = day + dayAdder + ' of ' + month + ' ' + mydate.getFullYear();
 
     // Create a new task object
 
